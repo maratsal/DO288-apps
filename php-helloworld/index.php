0fc0ee74269f231286ed3b26bcaee834f30d75b7
Marat Salakhutdinov <marat@salakhutdinov.com>
2022-04-11 15:24:11 -0400

    image 4

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,5 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "New line to generate image version 2\n";
 print "New line to generate image version 3\n";
+print "New line to generate image version 4\n";
 ?>
